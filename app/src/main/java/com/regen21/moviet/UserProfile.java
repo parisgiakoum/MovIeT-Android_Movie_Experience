20935816c423ab665d0c5d033504997326744688
Chatzigeorgiou Panagiotis <panoscha98@gmail.com>
2022-01-08 16:21:05 +0200

    Added UserProfile Activity and dummy UserProfile class for testing

---
+++ app/src/main/java/com/regen21/moviet/UserProfile.java
@@ -0,0 +1,16 @@
+package com.regen21.moviet;
+
+public class UserProfile {
+
+    private String username;
+    private int movies_watched;
+
+    public String getUsername() {
+        return username;
+    }
+
+    private int getMoviesWatched() {
+        return movies_watched;
+    }
+
+}
