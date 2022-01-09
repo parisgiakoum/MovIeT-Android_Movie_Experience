bb18c5bfcc8d27e80f018d6485f0d84250339705
Chatzigeorgiou Panagiotis <panoscha98@gmail.com>
2022-01-09 15:16:28 +0200

    Added dummy data to UserProfileActivity

--- app/src/main/java/com/regen21/moviet/UserProfile.java
+++ app/src/main/java/com/regen21/moviet/UserProfile.java
@@ -9,8 +9,13 @@ public class UserProfile {
         return username;
     }
 
-    private int getMoviesWatched() {
+    public int getMoviesWatched() {
         return movies_watched;
+    }
+
+    public void setUserData(String input_username, int input_movies) {
+        username = input_username;
+        movies_watched = input_movies;
     }
 
 }
