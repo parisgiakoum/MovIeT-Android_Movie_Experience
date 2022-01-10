fe98094d07fd9d5ca830d83cce48cc593dc65dba
danaekikue <danaekikue@gmail.com>
2022-01-10 19:24:31 +0200

    Image on click functionality to transist from home activity to movie activity

--- app/src/main/java/com/regen21/moviet/Home/RecyclerView/HomeViewHolder.java
+++ app/src/main/java/com/regen21/moviet/Home/RecyclerView/HomeViewHolder.java
@@ -3,9 +3,12 @@ package com.regen21.moviet.Home.RecyclerView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.regen21.moviet.Movie.MovieActivity;
 import com.regen21.moviet.Movie.MovieModel;
 import com.regen21.moviet.R;
 import com.squareup.picasso.Picasso;
@@ -20,7 +23,16 @@ public class HomeViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(MovieModel data) {
+        Context context = itemView.getContext();
         ImageView imageView = itemView.findViewById(R.id.home_movie_poster);
         Picasso.get().load(IMG_BASE_URL + "w342/" + data.getPoster_path()).into(imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MovieActivity.class);
+                intent.putExtra("movieID",data.getId());
+                context.startActivity(intent);
+            }
+        });
     }
 }
