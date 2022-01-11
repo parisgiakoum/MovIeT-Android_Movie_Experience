1c639866b28d54062bb453f233add0ddafe0e43d
Pavlos Paris Giakoumakis <parisgiakoum@gmail.com>
2022-01-11 20:41:46 +0200

    User login implemented

--- app/src/main/java/com/regen21/moviet/Authentication/LoginActivity.java
+++ app/src/main/java/com/regen21/moviet/Authentication/LoginActivity.java
@@ -1,23 +1,41 @@
 package com.regen21.moviet.Authentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.regen21.moviet.Home.HomeActivity;
 import com.regen21.moviet.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView create_newAccount;
+    private TextView create_newAccount;
+    private EditText editTextEmail, editTextPassword;
+    private Button login;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_constraint);
+
         create_newAccount=findViewById(R.id.create_account_linear);
+        login = findViewById(R.id.login_linear_login);
+        editTextEmail = findViewById(R.id.login_linear_username_value);
+        editTextPassword = findViewById(R.id.login_linear_password_value);
 
         create_newAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +44,59 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("E-mail is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Password should be more than 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    // redirect to home?
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
 
 }
