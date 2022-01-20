301208cfb8ca204fd74ed0b9b0300b6e5430decd
Pavlos Paris Giakoumakis <parisgiakoum@gmail.com>
2022-01-20 15:10:07 +0200

    Added functionality to change username

--- app/src/main/java/com/regen21/moviet/activities/UserActivity.java
+++ app/src/main/java/com/regen21/moviet/activities/UserActivity.java
@@ -20,10 +20,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,9 +47,18 @@ public class UserActivity extends AppCompatActivity {
         reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
 
+
         TextView textViewUsername, textViewEmail;
+        ImageButton imageButtonEdit, imageButtonConfirmEdit;
+        EditText editTextUsername;
+
         textViewUsername = findViewById(R.id.username);
         textViewEmail = findViewById(R.id.email);
+        imageButtonEdit = findViewById(R.id.edit_username_icon);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        imageButtonConfirmEdit = findViewById(R.id.confirm_username_icon);
+
+
 
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -59,6 +70,7 @@ public class UserActivity extends AppCompatActivity {
                     String email = userProfile.getEmail();
 
                     textViewUsername.setText(username);
+                    editTextUsername.setHint(username);
                     textViewEmail.setText(email);
                 }
             }
@@ -70,42 +82,39 @@ public class UserActivity extends AppCompatActivity {
         });
 
         // CHANGE USERNAME
-        ImageButton imageButtonEdit = findViewById(R.id.edit_username_icon);
         imageButtonEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reference.child(userID).child("username").setValue("paris").addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(UserActivity.this, "Username changed successfully!", Toast.LENGTH_LONG).show();
+                textViewUsername.setVisibility(View.GONE);
+                editTextUsername.setVisibility(View.VISIBLE);
+                imageButtonEdit.setVisibility(View.GONE);
+                imageButtonConfirmEdit.setVisibility(View.VISIBLE);
+            }
+        });
 
-                        // see changes from database
-                        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                User userProfile = snapshot.getValue(User.class);
+        imageButtonConfirmEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.child(userID).child("username").setValue(editTextUsername.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(UserActivity.this, "Username changed successfully!", Toast.LENGTH_LONG).show();
 
-                                if (userProfile != null) {
-                                    String username = userProfile.getUsername();
-                                    String email = userProfile.getEmail();
-
-                                    textViewUsername.setText(username);
-                                    textViewEmail.setText(email);
-                                }
-                            }
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-                                Toast.makeText(UserActivity.this, getString(R.string.error_default), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
+                }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(UserActivity.this, getString(R.string.error_default), Toast.LENGTH_LONG).show();
                     }
-                });;
+                });
+
+                textViewUsername.setVisibility(View.VISIBLE);
+                textViewUsername.setText(editTextUsername.getText());
+                imageButtonEdit.setVisibility(View.VISIBLE);
+                imageButtonConfirmEdit.setVisibility(View.GONE);
+                editTextUsername.setVisibility(View.GONE);
             }
         });
     }
+
 }
