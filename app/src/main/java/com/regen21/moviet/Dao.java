e58a4559ce71081265a81c45e6fbd50447a61f54
Pavlos Paris Giakoumakis <parisgiakoum@gmail.com>
2022-01-21 02:57:00 +0200

    Added movie info and movIeT btn in home recyclers - added blur in movieActivity back btn

--- app/src/main/java/com/regen21/moviet/Dao.java
+++ app/src/main/java/com/regen21/moviet/Dao.java
@@ -1,25 +1,14 @@
 package com.regen21.moviet;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.view.ViewParent;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.regen21.moviet.models.MovieModel;
-import com.regen21.moviet.models.User;
 
 public class Dao {
     private DatabaseReference userReference;
@@ -47,20 +36,6 @@ public class Dao {
     public Task<Void> removeMoviet(String movieId) {
         return userReference.child("MovIeTs").child(movieId).removeValue();
     }
-
-//    public boolean isInMoviet(String movieId) {
-//        userReference.child("MovIeTs").child(movieId).addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                return snapshot.child("MovIeT").child(movieId).exists();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        })
-//    }
 
 
 }
--- app/src/main/java/com/regen21/moviet/utils/HomeViewHolder.java
+++ app/src/main/java/com/regen21/moviet/utils/HomeViewHolder.java
@@ -6,8 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.regen21.moviet.Dao;
 import com.regen21.moviet.activities.MovieActivity;
 import com.regen21.moviet.models.MovieModel;
 import com.regen21.moviet.R;
@@ -17,6 +27,8 @@ public class HomeViewHolder extends RecyclerView.ViewHolder {
 
 
     public static final String IMG_BASE_URL = "https://image.tmdb.org/t/p/";
+    private boolean added = false;
+
 
     public HomeViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -24,6 +36,7 @@ public class HomeViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(MovieModel data) {
         Context context = itemView.getContext();
+
         ImageView imageView = itemView.findViewById(R.id.home_movie_poster);
         Picasso.get().load(IMG_BASE_URL + "w342/" + data.getPoster_path()).into(imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +47,85 @@ public class HomeViewHolder extends RecyclerView.ViewHolder {
                 context.startActivity(intent);
             }
         });
+
+        // Movie title
+        TextView textViewTitlePreview = itemView.findViewById(R.id.txt_movie_title_preview);
+        textViewTitlePreview.setText(data.getTitle());
+
+        // Year
+        TextView textViewYearPreview = itemView.findViewById(R.id.txt_year_preview);
+        if (data.getRelease_date().length() < 4) {
+            textViewYearPreview.setText("");
+        }
+        else
+        {
+            textViewYearPreview.setText(data.getRelease_date().substring(0, 4));
+        }
+
+        // Rating
+        RatingBar ratingBar = itemView.findViewById(R.id.rating_preview);
+        TextView textViewRatingPreview = itemView.findViewById(R.id.txt_rating_preview);
+
+        if(data.getVote_average()!=0) {
+            ratingBar.setVisibility(View.VISIBLE);
+            textViewRatingPreview.setVisibility(View.VISIBLE);
+            float normalizedRating = data.getVote_average() * 0.1f;
+            ratingBar.setRating(normalizedRating);
+            textViewRatingPreview.setText(String.valueOf(data.getVote_average()));
+        }
+        else {
+            ratingBar.setVisibility(View.INVISIBLE);
+            textViewRatingPreview.setVisibility(View.INVISIBLE);
+        }
+
+        // Add Movie Button
+        // MovIeT button
+        Button addIcon = itemView.findViewById(R.id.moviet_preview_btn);
+        Dao dao = new Dao();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            dao.getUserReference().child("MovIeTs").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.child(String.valueOf(data.getId())).exists()) {
+                        addIcon.setBackgroundResource(R.drawable.ic_baseline_check);
+                        added = true;
+                    } else {
+                        addIcon.setBackgroundResource(R.drawable.ic_baseline_library_add_24);
+                        added = false;
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+
+            addIcon.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (!added) {
+                        dao.addMoviet(data, context)
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(context, context.getString(R.string.error_default), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    } else {
+                        dao.removeMoviet(String.valueOf(data.getId()))
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(context, context.getString(R.string.error_default), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    }
+                }
+            });
+        }
+        else {
+            addIcon.setVisibility(View.GONE);
+            itemView.findViewById(R.id.view2).setVisibility(View.GONE);
+        }
     }
 }
